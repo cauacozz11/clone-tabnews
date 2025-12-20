88aadfe957ca99e239a6a6717399ac7d67cab064
cauacozz11 <cauacozz@gmail.com>
2025-12-20 17:27:58 +0000

    ajuste no modo de importação da função

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 test("Validação de usuário", () => {
   const aluno = calculadora.login("cauã");
