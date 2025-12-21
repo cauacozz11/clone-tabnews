f7a0203bfde38ac3438e6fe4e8b11891d4680274
cauacozz11 <cauacozz@gmail.com>
2025-12-21 17:04:20 +0000

    foi feito o primeiro teste de requisição recebendo um status 200 como esperado

--- pages/api/status.js
+++
@@ -1,9 +0,0 @@
-function status(request, response) {
-  response.status(200).json({
-    status: "healthy",
-    updated_at: new Date().toISOString(),
-    message: "O sistema está operando normalmente.",
-  });
-}
-
-export default status;
--- tests/calculadora.test.js
+++
@@ -1,6 +0,0 @@
-const calculadora = require("../models/calculadora.js");
-
-test("Validação de usuário", () => {
-  const aluno = calculadora.login("cauã");
-  expect(aluno).toBe("Usuário não está cadastrado em nosso sistema");
-});
