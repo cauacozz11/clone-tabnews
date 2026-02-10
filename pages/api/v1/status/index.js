93f3429ee71e38368fd168c3ad07eaee15f4ebbf
cauacozz11 <cauacozz@gmail.com>
2026-02-10 23:03:02 +0000

    adiciona umas alterações em arquivos de teste para  subir banco de dados de forma remota

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,14 @@
-function status(request, response) {
-  response.status(200).json({ chave: "Deu boa a requisição verifica pra mim" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  try {
+    const result = await database.query("SELECT 1 + 1 as SUM;");
+    console.log(result.rows);
+    response
+      .status(200)
+      .json({ chave: "Deu boa a requisição verifica pra mim" });
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 export default status;
